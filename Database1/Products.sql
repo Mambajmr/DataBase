873d7c0f34eb05f5fd9de4d346fb2cc80a947909
Артем Mamba <Krabovich05@mail.ru>
2022-07-14 20:16:46 +0300

    Добавьте файлы проекта.

---
+++ Database1/Products.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE Products (
+	id int PRIMARY KEY,
+	name varchar(255) NOT NULL,
+);
+
+INSERT INTO Products
+VALUES  (1, 'Хлеб'),
+	    (2, 'Молоко'),
+	    (3, 'Туалетная бумага'),
+        (4, 'Роза чайная');
